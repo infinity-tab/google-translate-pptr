0a54c2b253155ac6bcdf73d2531aab2e018f8744
大天蓬 <suiyuex@qq.com>
2022-04-22 16:44:36 +0800

    feat: to lib

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # google-translate-pptr
-Google translate api based on puppeteer
+
+```javascript
+
+import Translate from "google-translate-pptr";
+
+
+const start = async ()=>{
+  // must initialize first
+  const translate = await Translate()
+
+
+  await translate("Hello", {from: 'en', to: 'zh-CN'})
+  // 你好
+  
+}
+
+start()
+
+```
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,99 +1,47 @@
 import puppeteer from "puppeteer";
-import Fastify from "fastify";
-import fCors from "fastify-cors";
 import PagePool from "./pagePool.js";
 
-const fastify = Fastify({ logger: true });
+const start = async (pageCount = 10, headless = true) => {
+  const browser = await puppeteer.launch({ headless });
+  const pagePool = new PagePool(browser, pageCount);
+  await pagePool.init();
 
-fastify.register(fCors, {});
+  /**
+   * @param { string } text
+   * @param { { from: string, to: string } } param2
+   * @returns { Promise<{ from: string, to: string, text: string }> }
+   */
+  return async function (text, { from, to }) {
+    if (from === to) {
+      return { from, to, text };
+    }
+    const page = await pagePool.getPage();
 
-const pageCount = 5;
-const headless = true;
+    await page.evaluate(
+      ([from, to, text]) => {
+        location.href = `?sl=${from}&tl=${to}&text=${encodeURIComponent(text)}`;
+      },
+      [from, to, text]
+    );
+    // translating...
+    await page.waitForSelector(`span[lang=${to}]`);
 
-const start = async () => {
-  try {
-    const browser = await puppeteer.launch({ headless });
-    const pagePool = new PagePool(browser, pageCount);
-    await pagePool.init();
+    /** @type { string } */
+    let transResult = await page.evaluate(
+      (to) =>
+        // @ts-expect-error
+        document.querySelectorAll(`span[lang=${to}]`)[0].innerText,
+      to
+    );
 
-    fastify.post("/translate", async (request, reply) => {
-      const { items, str } = request.body;
+    if (transResult.endsWith(".")) {
+      transResult = transResult.replace(/\.$/, "");
+    }
 
-      /** @type { Array<string> }  */
-      const toArr = Object.keys(items);
-      const from = Object.values(items)[0];
-      const toLen = toArr.length;
+    pagePool.releasePage(page);
 
-      const result = [];
-
-      const tasks = [];
-      for (let i = 0; i < toLen; ++i) {
-        tasks.push(toTranslate(pagePool, { from, to: toArr[i], text: str }));
-      }
-
-      const transArr = await Promise.all(tasks);
-      result.push(...transArr);
-
-      const data = result.reduce((_, [to, text]) => {
-        _[to] = text;
-        return _;
-      }, {});
-
-      return {
-        code: 0,
-        data,
-        totalCount: toArr.length,
-        count: result.length,
-      };
-    });
-
-    await fastify.listen(3000);
-    console.info(`server is run on 127.0.0.1:${3000}}`);
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
+    return { text: transResult, from, to };
+  };
 };
 
-start();
-
-/**
- *
- * @param { PagePool } pool
- * @param { { from: string, to: string, text: string } } param1
- * @returns { Promise<[string, string]> } // tuple
- */
-async function toTranslate(pool, { from, to, text }) {
-  if (from === to) {
-    return [to, text];
-  }
-
-  const page = await pool.getPage();
-
-  await page.evaluate(
-    ([from, to, text]) => {
-      location.href = `?sl=${from}&tl=${to}&text=${encodeURIComponent(text)}`;
-    },
-    [from, to, text]
-  );
-
-  // translating...
-  await page.waitForSelector(`span[lang=${to}]`);
-
-  /** @type { string } */
-  let transResult = await page.evaluate(
-    (to) =>
-      // @ts-expect-error
-      document.querySelectorAll(`span[lang=${to}]`)[0].innerText,
-    to
-  );
-
-  if (transResult.endsWith(".")) {
-    transResult = transResult.replace(/\.$/, "");
-  }
-
-  pool.releasePage(page);
-
-  // get translated text
-  return [to, transResult];
-}
+export default start;
